5ae09958e7f1fd35157db66c7551e449c48d952d
Ayida Aman <amanayida1@gmail.com>
2026-07-21 01:08:34 -0700

    shallow vs deep arrays

--- NumPy_Arrays/task.py
+++ NumPy_Arrays/task.py
@@ -6,3 +6,16 @@ print(repr(arr))
 
 arr = np.array([0, 0.1, 2])
 print(repr(arr))
+
+# Similar to Python lists, when we make a reference to a NumPy array it doesn't create a different array.
+
+a = np.array([0, 1])
+b = np.array([9, 8])
+c = a
+print('Array a: {}'.format(repr(a)))
+c[0] = 5
+print('Array a: {}'.format(repr(a)))
+
+d = b.copy()
+d[0] = 6
+print('Array b: {}'.format(repr(b)))
